d45f99014abec72c72edcd8d07207ca4672583f2
Gregory Conrad <gregorysconrad@gmail.com>
2023-08-13 11:07:45 -0700

    style: add use for the builtin side effects

--- packages/rearch/lib/src/side_effects.dart
+++ packages/rearch/lib/src/side_effects.dart
@@ -1,25 +1,31 @@
 import 'dart:async';
 
+import 'package:meta/meta.dart';
 import 'package:rearch/rearch.dart';
+
+extension _UseConvenience on SideEffectRegistrar {
+  SideEffectRegistrar get use => this;
+}
 
 /// A collection of builtin side effects.
 extension BuiltinSideEffects on SideEffectRegistrar {
   /// Convenience side effect that gives a copy of the [SideEffectApi].
-  SideEffectApi api() => register((api) => api);
+  SideEffectApi api() => use.register((api) => api);
 
   /// Convenience side effect that gives a copy of [SideEffectApi.rebuild].
-  void Function() rebuilder() => api().rebuild;
+  void Function() rebuilder() => use.api().rebuild;
 
   /// Side effect that calls the supplied [callback] once, on the first build.
-  T callonce<T>(T Function() callback) => register((_) => callback());
+  T callonce<T>(T Function() callback) => use.register((_) => callback());
 
   /// Returns a raw value wrapper; i.e., a getter and setter for some value.
   /// *The setter will not trigger rebuilds*.
   /// The initial state will be set to the result of running [init],
   /// if it was provided. Otherwise, you must manually set it
   /// via the setter before ever calling the getter.
+  @experimental
   (T Function(), void Function(T)) rawValueWrapper<T>([T Function()? init]) {
-    return callonce(() {
+    return use.callonce(() {
       late T state;
       if (init != null) state = init();
       return (() => state, (T newState) => state = newState);
@@ -34,7 +40,7 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   /// see https://react.dev/reference/react/useState
   (T Function(), void Function(T)) lazyStateGetterSetter<T>(T Function() init) {
     // We use register directly to keep the same setter function across rebuilds
-    return register((api) {
+    return use.register((api) {
       var state = init();
 
       T getter() => state;
@@ -54,38 +60,38 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   /// Similar to the `useState` hook from React;
   /// see https://react.dev/reference/react/useState
   (T Function(), void Function(T)) stateGetterSetter<T>(T initial) =>
-      lazyStateGetterSetter(() => initial);
+      use.lazyStateGetterSetter(() => initial);
 
   /// Side effect that provides a way for capsules to contain some state,
   /// where the initial state is computationally expensive.
   /// Similar to the `useState` hook from React;
   /// see https://react.dev/reference/react/useState
   (T, void Function(T)) lazyState<T>(T Function() init) {
-    final (getter, setter) = lazyStateGetterSetter(init);
+    final (getter, setter) = use.lazyStateGetterSetter(init);
     return (getter(), setter);
   }
 
   /// Side effect that provides a way for capsules to contain some state.
   /// Similar to the `useState` hook from React;
   /// see https://react.dev/reference/react/useState
-  (T, void Function(T)) state<T>(T initial) => lazyState(() => initial);
+  (T, void Function(T)) state<T>(T initial) => use.lazyState(() => initial);
 
   /// Side effect that provides a way for capsules to hold onto some value
   /// between builds, where the initial value is computationally expensive.
   /// Similar to the `useRef` hook from React;
   /// see https://react.dev/reference/react/useRef
-  T lazyValue<T>(T Function() init) => callonce(init);
+  T lazyValue<T>(T Function() init) => use.callonce(init);
 
   /// Side effect that provides a way for capsules to hold onto some value
   /// between builds.
   /// Similar to the `useRef` hook from React;
   /// see https://react.dev/reference/react/useRef
-  T value<T>(T initial) => lazyValue(() => initial);
+  T value<T>(T initial) => use.lazyValue(() => initial);
 
   /// Returns the previous value passed into [previous],
   /// or `null` on first build.
   T? previous<T>(T current) {
-    final (getter, setter) = rawValueWrapper<T?>(() => null);
+    final (getter, setter) = use.rawValueWrapper<T?>(() => null);
     final prev = getter();
     setter(current);
     return prev;
@@ -96,13 +102,13 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   /// Useful for a capsule wishing to read its own current data,
   /// as capsules cannot read their own data on their first build
   /// (as it won't exist yet).
-  bool isFirstBuild() => previous(false) ?? true;
+  bool isFirstBuild() => use.previous(false) ?? true;
 
   /// Equivalent to the `useMemo` hook from React.
   /// See https://react.dev/reference/react/useMemo
   T memo<T>(T Function() memo, [List<Object?> dependencies = const []]) {
-    final oldDependencies = previous(dependencies);
-    final (getData, setData) = rawValueWrapper<T>();
+    final oldDependencies = use.previous(dependencies);
+    final (getData, setData) = use.rawValueWrapper<T>();
     if (_didDepsListChange(dependencies, oldDependencies)) {
       setData(memo());
     }
@@ -115,10 +121,10 @@ extension BuiltinSideEffects on SideEffectRegistrar {
     void Function()? Function() effect, [
     List<Object?>? dependencies,
   ]) {
-    final oldDependencies = previous(dependencies);
+    final oldDependencies = use.previous(dependencies);
     final (getDispose, setDispose) =
-        rawValueWrapper<void Function()?>(() => null);
-    register((api) => api.registerDispose(() => getDispose()?.call()));
+        use.rawValueWrapper<void Function()?>(() => null);
+    use.register((api) => api.registerDispose(() => getDispose()?.call()));
 
     if (dependencies == null ||
         _didDepsListChange(dependencies, oldDependencies)) {
@@ -135,7 +141,7 @@ extension BuiltinSideEffects on SideEffectRegistrar {
     Reducer<State, Action> reducer,
     State initialState,
   ) {
-    final (currState, setState) = state(initialState);
+    final (currState, setState) = use.state(initialState);
     return (
       currState,
       (action) => setState(reducer(currState, action)),
@@ -155,8 +161,8 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   /// Or, if possible, it is even better to wrap the future in an entirely
   /// new capsule (although this is not always possible).
   AsyncValue<T> future<T>(Future<T> future) {
-    final asStream = memo(future.asStream, [future]);
-    return stream(asStream);
+    final asStream = use.memo(future.asStream, [future]);
+    return use.stream(asStream);
   }
 
   /// Consumes a [Stream] and watches the given stream.
@@ -169,16 +175,16 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   /// Or, if possible, it is even better to wrap the stream in an entirely
   /// new capsule (although this is not always possible).
   AsyncValue<T> stream<T>(Stream<T> stream) {
-    final rebuild = rebuilder();
-    final (getValue, setValue) = rawValueWrapper<AsyncValue<T>>(
+    final rebuild = use.rebuilder();
+    final (getValue, setValue) = use.rawValueWrapper<AsyncValue<T>>(
       () => AsyncLoading<T>(None<T>()),
     );
 
     final (getSubscription, setSubscription) =
-        rawValueWrapper<StreamSubscription<T>?>(() => null);
-    effect(() => getSubscription()?.cancel, [getSubscription()]);
+        use.rawValueWrapper<StreamSubscription<T>?>(() => null);
+    use.effect(() => getSubscription()?.cancel, [getSubscription()]);
 
-    final oldStream = previous(stream);
+    final oldStream = use.previous(stream);
     final needToInitializeState = stream != oldStream;
 
     if (needToInitializeState) {
@@ -214,9 +220,9 @@ extension BuiltinSideEffects on SideEffectRegistrar {
     required Future<T> Function() read,
     required Future<void> Function(T) write,
   }) {
-    final readFuture = callonce(read);
-    final readState = future(readFuture);
-    final (state: writeState, :mutate, clear: _) = mutation<T>();
+    final readFuture = use.callonce(read);
+    final readState = use.future(readFuture);
+    final (state: writeState, :mutate, clear: _) = use.mutation<T>();
 
     final state = (writeState ?? readState).fillInPreviousData(readState.data);
 
@@ -238,15 +244,16 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   /// Note: `mutate()` and `clear()` *should not* be called directly from
   /// within build, but rather from within some callback.
   Mutation<T> mutation<T>() {
-    final rebuild = rebuilder();
-    final (getValue, setValue) = rawValueWrapper<AsyncValue<T>?>(() => null);
+    final rebuild = use.rebuilder();
+    final (getValue, setValue) =
+        use.rawValueWrapper<AsyncValue<T>?>(() => null);
 
     // We convert to a stream here because we can cancel a stream subscription;
     // there is no builtin way to cancel a future.
-    final (future, setFuture) = state<Future<T>?>(null);
-    final asStream = memo(() => future?.asStream(), [future]);
+    final (future, setFuture) = use.state<Future<T>?>(null);
+    final asStream = use.memo(() => future?.asStream(), [future]);
 
-    effect(
+    use.effect(
       () {
         setValue(
           asStream == null ? null : AsyncLoading(getValue()?.data ?? None<T>()),
@@ -288,8 +295,8 @@ extension BuiltinSideEffects on SideEffectRegistrar {
   (AsyncValue<T>, void Function()) refreshableFuture<T>(
     Future<T> Function() futureFactory,
   ) {
-    final (currFuture, setFuture) = lazyState(futureFactory);
-    final futureState = future(currFuture);
+    final (currFuture, setFuture) = use.lazyState(futureFactory);
+    final futureState = use.future(currFuture);
     return (futureState, () => setFuture(futureFactory()));
   }
 }

a38ca2b51d3a610f32b3cbf733ac89bd806ad421
Vicente <busslina@gmail.com>
2024-04-16 20:15:15 +0200

    snapshot

--- packages/react_rearch/lib/src/widgets/consumer.dart
+++ packages/react_rearch/lib/src/widgets/consumer.dart
@@ -74,7 +74,6 @@ class _ComponentSideEffectApiProxyImpl implements ComponentSideEffectApi {
       if (isCanceled) return;
     }
 
-    // component.markNeedsBuild();
     component
       ..debug('forceUpdate()')
       ..forceUpdate();

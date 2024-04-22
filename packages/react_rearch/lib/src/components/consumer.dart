1eb8ca7e11df64677a873189f3096bc7b8d611a9
Vicente <busslina@gmail.com>
2024-04-22 19:09:26 +0200

    snapshot

--- packages/react_rearch/lib/src/components/consumer.dart
+++ packages/react_rearch/lib/src/components/consumer.dart
@@ -82,7 +82,12 @@ abstract class RearchComponent extends Component2
     //   forceUpdate();
     // });
 
-    Future.delayed(const Duration(milliseconds: 1000), () {
+    // Future.delayed(const Duration(milliseconds: 1000), () {
+    //   if (_unmounting) return;
+    //   forceUpdate();
+    // });
+
+    Future.delayed(Duration.zero, () {
       if (_unmounting) return;
       forceUpdate();
     });

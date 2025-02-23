f6b29fc580234dacb6de8550b57e97c09f9074da
Gregory Conrad <gregorysconrad@gmail.com>
2025-02-22 23:14:42 -0500

    chore: add nix integration

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "ReArch for Dart";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            flutter
+            melos
+          ];
+        };
+      }
+    );
+}
